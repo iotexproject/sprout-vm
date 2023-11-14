e5e6a623e9ef2d5141673c95fae1f3c12b2ef404
hunshenshi <289517357@qq.com>
2023-11-14 16:06:06 +0800

    feat(grpc): add demo code in grpc

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const protoLoader = require('@grpc/proto-loader');
 const wasm = require("./wasm_instance")
 const PROTO_PATH = './proto/vm_runtime.proto'
 
+// TODO add map to store wasm instance(instanceId: wasmInstance/bytes)
+// need to update wasm_instance.js? add getWasmInstance 
+
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
     keepCase: true,
     longs: String,
@@ -14,12 +17,34 @@ const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
 const vmRuntime = grpc.loadPackageDefinition(packageDefinition).vm_runtime;
 
 function create(call, callback) {
+    const project = call.request.project;
+    // const bytes = call.request.content;
+    const path = require('path').join(__dirname, '../wasm/halo2_evm_verifier_bg.wasm');
+    const bytes = require('fs').readFileSync(path);
+    wasm.setWasmBytes(bytes);
+    wasm.initWasmInstance();
 
-    callback(null, { instanceId: 'Hello ' + call.request.project });
+    callback(null, { instanceId: 'Hello ' + project });
 }
 
 function executeOperator(call, callback) {
-    callback(null, { message: 'Hello ' + call.request.name });
+    const project = call.request.project;
+    const param = call.request.param;
+
+    const path = require('path').join(__dirname, '../wasm/halo2_evm_verifier_bg.wasm');
+    const bytes = require('fs').readFileSync(path);
+    wasm.setWasmBytes(bytes);
+    wasm.initWasmInstance();
+
+    const result = wasm.prove(param);
+    console.log(result);
+    // convert result to bytes
+    let resultBytes = new Uint8Array(result.length);
+    for (var i = 0; i < result.length; i++) {
+        resultBytes[i] = result.charCodeAt(i);
+    }
+
+    callback(null, { result: resultBytes });
 }
 
 function startGrpcServer() {
